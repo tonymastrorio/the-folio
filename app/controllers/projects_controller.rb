b8dafbef7c0301bf20cbc43923f89bbac976a9e7
Tony <github email address>
2017-11-09 14:11:02 -0500

    add project edit page

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -19,11 +19,32 @@ class ProjectsController < ApplicationController
 
     def show
         @project = Project.find_by(id: params[:id])
+        @portfolio = Portfolio.find_by(id: params[:portfolio_id])
+    end
+
+    def edit
+        require_permission_projects_controller
+        @project = Project.find_by(id: params[:id])
+        @portfolio = Portfolio.find_by(id: params[:portfolio_id])
+    end
+
+    def update
+        require_permission_projects_controller
+        @portfolio = Portfolio.find_by(id: params[:portfolio_id])
+        @project = Project.update(project_params)
+
+        redirect_to portfolio_project_path(@project)
     end
 
     private
 
     def project_params
         params.require(:project).permit(:portfolio_id, :image, :title, :description, :github_url, :skill_ids => [], :skills_attributes => {})
+    end
+
+    def require_permission_projects_controller
+        if current_user != Portfolio.find(params[:portfolio_id]).user
+            redirect_to root_path
+        end
     end
 end
--- notes.md
+++ notes.md
@@ -1,5 +1,5 @@
 Next steps:
 
-1. Add button for project show page on portfolio show page
+1. Add button for project show page on portfolio show page - DONE
 
-2. Add project edit page
+2. Add project edit page - DONE
