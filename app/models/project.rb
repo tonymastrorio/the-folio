3a32aee537b565abe07d55d353013799ddde3704
Tony <github email address>
2017-11-13 13:23:59 -0500

    add form validations, custom attribute writer

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,9 +2,21 @@ class Project < ApplicationRecord
     belongs_to :portfolio
     has_many :project_skills
     has_many :skills, through: :project_skills
-    accepts_nested_attributes_for :skills, allow_destroy: true, reject_if: :all_blank
+    # accepts_nested_attributes_for :skills, allow_destroy: true, reject_if: :all_blank
 
     has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/assets/:style/missing.png"
     validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
     validates_presence_of :title, :description
+
+    def skills_attributes=(skills_attributes)
+        skills_attributes.values.each do |skill_attributes|
+            #Do not create a category if it does not have a name
+            if skill_attributes[:name].present?
+                skill = Skill.find_or_create_by(name: skill_attributes[:name])
+                if !self.skills.include?(skill)
+                    self.project_skills.build(:skill => skill)
+                end
+            end
+        end
+    end
 end
