02dab5387d2381cee1b3b3c585b303282458eacd
Tony <github email address>
2017-11-22 10:48:39 -0500

    handle already created omniauth user

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,11 +1,20 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def facebook
-     @user = User.from_omniauth(request.env["omniauth.auth"])
-     sign_in_and_redirect @user      
+        @user = User.from_omniauth(request.env["omniauth.auth"])
+        if @user.errors.empty?
+            sign_in_and_redirect @user
+        else
+            render "/devise/registrations/new"
+        end
   end
 
   def github
     @user = User.from_omniauth(request.env["omniauth.auth"])
-     sign_in_and_redirect @user  
+    if @user.errors.empty?
+        sign_in_and_redirect @user
+
+    else
+        render "/devise/registrations/new"  
+    end
   end
 end
